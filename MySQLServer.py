191f9688618fc24828b64a360a2c352d55b0420d
Orit Samuel <oritsamuelseid@gmail.com>
2024-10-20 20:13:57 +0300

    script

--- MySQLServer.py
+++ MySQLServer.py
@@ -9,7 +9,7 @@ def create_database(database_name):
   try:
     connection = mysql.connector.connect(host=HOST, user=USERNAME, password=PASSWORD)
     cursor = connection.cursor()
-    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
+    cursor.execute("CREATE DATABASE IF NOT EXISTS `alx_book_store`;")
     connection.commit()
     print(f"Database '{database_name}' created successfully!")
   except mysql.connector.Error as err:
